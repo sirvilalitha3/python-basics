7e15faed5a163c3b88a2ed3e3bd04fecb26c732e
sirvilalitha3 <sirvilalitha3@gmail.com>
2026-08-20 20:10:56 +0530

    Delete Read_file.py

--- Read_file.py
+++
@@ -1,6 +0,0 @@
-with open("sample.txt","r") as file:
-    #data=file.read() to read entire content in file
-    #data=file.read(1) to read specific characters
-    #data=file.readline() to read single line
-    data=file.readlines()
-    print(data)
